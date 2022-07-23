c19f092215e8d5b1688677204e927d76446205ad
4hsnim <4hsnim@naver.com>
2022-07-24 01:25:18 +0900

    feature: 무한스크롤 기능 구현

--- src/pages/AllListPage.js
+++ src/pages/AllListPage.js
@@ -1,29 +1,68 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import InfiniteScroll from "react-infinite-scroll-component";
-
-import { loadCollection } from "../redux/modules/collectionSlice";
+import { serverUrl } from "../shared/api";
 import CardAuction from "../components/CardAuction";
 import CardCollection from "../components/CardCollection";
 import CardItem from "../components/CardItem";
+import axios from "axios";
 
 const AllListPage = () => {
   const [category, setCategory] = useState(0);
   const dispatch = useDispatch();
-  const [items, setItems] = useState("");
+  const [collectionData, setCollectionData] = useState([]);
+  const [hasMore, sethasMore] = useState(true);
+  const [page, setpage] = useState(2);
 
-  const fetchData = () => {};
-
+  // useEffect(() => {
+  //   dispatch(loadCollection());
+  // }, []);
+  // `http://localhost:5001/collections?_page=1&_limit=12`
+  // `${serverUrl}/api/explore?tab=collection&_page=1&_limit=12`
   useEffect(() => {
-    dispatch(loadCollection());
+    const loadFirstCollection = async () => {
+      const response = await axios(
+        `${serverUrl}/api/explore?tab=collection&_page=1&_limit=12`
+      )
+        .then((response) => {
+          console.log(response.data);
+          setCollectionData(response.data.data);
+          return response.data.data;
+        })
+        .catch((error) => {
+          console.log(error.message);
+        });
+    };
+    loadFirstCollection();
   }, []);
 
-  const collectionList = useSelector((state) => state.collection.collection);
-  console.log(collectionList);
-  if (!collectionList) {
+  const fetchCollection = async () => {
+    const response = await axios
+      .get(`${serverUrl}/api/explore?tab=collection&_page=${page}&_limit=12`)
+      .then((response) => {
+        console.log(response.data);
+        return response.data.data;
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+    return response;
+  };
+
+  const fetchData = async () => {
+    const commentsFormServer = await fetchCollection();
+
+    setCollectionData([...collectionData, ...commentsFormServer]);
+    if (commentsFormServer.length === 0 || commentsFormServer.length < 12) {
+      sethasMore(false);
+    }
+    setpage(page + 1);
+  };
+
+  if (!collectionData) {
     return null;
   }
-  console.log(collectionList);
+  console.log(collectionData);
 
   return (
     <div className="Container">
@@ -53,28 +92,26 @@ const AllListPage = () => {
           경매 진행중
         </button>
       </div>
-      <div className="CardWrapper">
-        {
-          category === 0 && (
-            //   <InfiniteScroll
-            //   dataLength={items.length} //This is important field to render the next data
-            //   next={fetchData}
-            //   hasMore={true}
-            //   loader={<h4>Loading...</h4>}
-            //   endMessage={
-            //     <p style={{ textAlign: 'center' }}>
-            //       <b>Yay! You have seen it all</b>
-            //     </p>
-            //   }
 
-            // >
-            <CardCollection data={collectionList} />
-          )
-          // </InfiniteScroll>
-        }
-        {category === 1 && <CardItem />}
-        {category === 2 && <CardAuction />}
-      </div>
+      {category === 0 && (
+        <InfiniteScroll
+          dataLength={collectionData.length} //This is important field to render the next data
+          next={fetchData}
+          hasMore={hasMore}
+          loader={<h4>Loading...</h4>}
+          endMessage={
+            <p style={{ textAlign: "center" }}>
+              <b>Yay! You have seen it all</b>
+            </p>
+          }
+        >
+          <div className="CardWrapper">
+            <CardCollection data={collectionData} />
+          </div>
+        </InfiniteScroll>
+      )}
+      {category === 1 && <CardItem />}
+      {category === 2 && <CardAuction />}
     </div>
   );
 };
